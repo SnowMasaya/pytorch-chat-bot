ff08ba503165e622f6b4fc78fb224a00c787591e
SnowGushiGit <snow.akogi.pgel@gmail.com>
2017-12-03 10:29:06 +0900

    # Update attention visualize
    For japanese font

--- src/visualization/attention_visualize.py
+++ src/visualization/attention_visualize.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import torch
+from matplotlib.font_manager import FontProperties
 
 
 class AttentionVisualize(object):
@@ -17,12 +18,13 @@ class AttentionVisualize(object):
         self.fig = plt.figure()
 
     def visualize(self, input_words, output_words, attentions):
+        fp = FontProperties(fname='visualization/conf/ipag.ttf', size=14)
         ax = self.fig.add_subplot(111)
-        cax = ax.matshow(attentions.numpy(), cmap='done')
+        cax = ax.matshow(attentions.numpy(), cmap='bone')
         self.fig.colorbar(cax)
 
-        ax.set_xticklabels([''] + input_words, rotation=90)
-        ax.set_yticklabels([''] + output_words)
+        ax.set_xticklabels([''] + input_words, rotation=90, fontproperties=fp)
+        ax.set_yticklabels([''] + output_words, fontproperties=fp)
 
         ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
         ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
